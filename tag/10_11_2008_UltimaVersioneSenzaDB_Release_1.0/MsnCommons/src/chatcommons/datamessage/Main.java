4c2ed5a226fba5285e8502645642f0c881f74597
Administrator <Administrator@2a2a0caa-3540-4b53-89ca-458ca4f19e0f>
2008-11-09 23:56:18 +0000

    made a copy
    git-svn-id: http://svn.luigiantonini.it/Messenger@467 2a2a0caa-3540-4b53-89ca-458ca4f19e0f

---
+++ tag/10_11_2008_UltimaVersioneSenzaDB_Release_1.0/MsnCommons/src/chatcommons/datamessage/Main.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package chatcommons.datamessage;
+
+/**
+ *
+ * @author Luigi
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+//        ObjectFactory  factory = new ObjectFactory();
+//        factory.createMESSAGEParametersParameter().set
+                
+    }
+
+}
---
+++ tag/10_11_2008_UltimaVersioneSenzaDB_Release_1.0/MsnCommons/src/chatcommons/datamessage/MessageManger.java
@@ -0,0 +1,299 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chatcommons.datamessage;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.StringReader;
+import java.io.UnsupportedEncodingException;
+import java.net.SocketException;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import javax.xml.transform.stream.StreamSource;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import static chatcommons.Commands.*;
+
+/**
+ *
+ * @author Luigi
+ */
+public class MessageManger {
+
+    private static Log log = LogFactory.getLog(MessageManger.class);
+    private static String DEFAULTENCODING = "UTF-8";
+
+    public static boolean directWriteMessage(MESSAGE message, OutputStream os) throws SocketException {
+
+        try {
+            javax.xml.bind.JAXBContext jaxbCtx = javax.xml.bind.JAXBContext.newInstance(message.getClass().getPackage().getName());
+            javax.xml.bind.Marshaller marshaller = jaxbCtx.createMarshaller();
+            marshaller.setProperty(javax.xml.bind.Marshaller.JAXB_ENCODING, "UTF-8"); //NOI18N
+
+            marshaller.setProperty(javax.xml.bind.Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.FALSE);
+            synchronized (os) {
+                marshaller.marshal(message, os);
+                try {
+                    os.write("\n".getBytes(DEFAULTENCODING));
+                } catch (IOException ex) {
+                    log.debug(ex);
+                }
+            }
+        } catch (javax.xml.bind.JAXBException ex) {
+            // XXXTODO Handle exception
+            log.error(ex); //NOI18N
+            throw new SocketException();
+
+        }
+
+        return false;
+    }
+
+    public static String messageToString(MESSAGE message) {
+        String toReturn = "";
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+
+        try {
+            javax.xml.bind.JAXBContext jaxbCtx = javax.xml.bind.JAXBContext.newInstance(message.getClass().getPackage().getName());
+            javax.xml.bind.Marshaller marshaller = jaxbCtx.createMarshaller();
+            marshaller.setProperty(javax.xml.bind.Marshaller.JAXB_ENCODING, "UTF-8"); //NOI18N
+
+            marshaller.setProperty(javax.xml.bind.Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.FALSE);
+            marshaller.marshal(message, baos);
+//            marshaller.marshal(message, System.out);
+        } catch (javax.xml.bind.JAXBException ex) {
+            // XXXTODO Handle exception
+            log.error(ex.getMessage()); //NOI18N
+
+        }
+        try {
+
+
+            String xml = baos.toString(DEFAULTENCODING);
+            toReturn = xml + "\n";
+
+
+//            log.debug("xml to send : " + toReturn);
+            return toReturn;
+
+
+        } catch (Exception ex) {
+            log.error(ex);
+            return null;
+        }
+    }
+
+    public static String messageToStringFormatted(MESSAGE message) {
+        String toReturn = "";
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+
+        try {
+            javax.xml.bind.JAXBContext jaxbCtx = javax.xml.bind.JAXBContext.newInstance(message.getClass().getPackage().getName());
+            javax.xml.bind.Marshaller marshaller = jaxbCtx.createMarshaller();
+            marshaller.setProperty(javax.xml.bind.Marshaller.JAXB_ENCODING, "UTF-8"); //NOI18N
+
+            marshaller.setProperty(javax.xml.bind.Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+            marshaller.marshal(message, baos);
+//            marshaller.marshal(message, System.out);
+        } catch (javax.xml.bind.JAXBException ex) {
+            // XXXTODO Handle exception
+            log.error(ex.getMessage()); //NOI18N
+
+        }
+        try {
+
+            String xml = baos.toString("UTF-8");
+            toReturn = "\n" + xml;
+
+
+//            log.debug("xml to send : " + toReturn);
+            return toReturn;
+
+
+
+        } catch (UnsupportedEncodingException ex) {
+            log.error(ex);
+            return null;
+        }
+    }
+
+    public static MESSAGE parseXML(String xml) {
+        ObjectFactory objectFactory = new ObjectFactory();
+        MESSAGE toReturn = objectFactory.createMESSAGE();
+
+
+        try {
+            javax.xml.bind.JAXBContext jaxbCtx = javax.xml.bind.JAXBContext.newInstance(toReturn.getClass().getPackage().getName());
+            javax.xml.bind.Unmarshaller unmarshaller = jaxbCtx.createUnmarshaller();
+            toReturn = (MESSAGE) unmarshaller.unmarshal(new StreamSource(new StringReader(xml))); //NOI18N
+
+            return toReturn;
+        } catch (javax.xml.bind.JAXBException ex) {
+            // XXXTODO Handle exception
+            log.error(ex); //NOI18N
+
+            return null;
+        }
+
+    }
+
+    public static MESSAGE createRequest(String name, List<String> receivers, Map<String, String> parameters) {
+        ObjectFactory objectFactory = new ObjectFactory();
+        MESSAGE request = objectFactory.createMESSAGE();
+        request.setParameters(objectFactory.createMESSAGEParameters());
+        request.setReceivers(objectFactory.createMESSAGEReceivers());
+        request.setType(REQUEST);
+        if (name != null) {
+            request.setName(name);
+        }
+        if (receivers != null) {
+            for (String receiver : receivers) {
+                request.getReceivers().getReceiver().add(receiver);
+            }
+        }
+        if (parameters != null) {
+
+            Set<String> paramKeys = parameters.keySet();
+            for (String key : paramKeys) {
+                MESSAGE.Parameters.Parameter msgParamter = objectFactory.createMESSAGEParametersParameter();
+                msgParamter.setName(key);
+                msgParamter.setValue(parameters.get(key));
+                request.getParameters().getParameter().add(msgParamter);
+            }
+        }
+        return request;
+    }
+
+    public static MESSAGE createCommand(String name, Map<String, String> parameters) {
+        ObjectFactory objectFactory = new ObjectFactory();
+        MESSAGE request = objectFactory.createMESSAGE();
+        request.setParameters(objectFactory.createMESSAGEParameters());
+        request.setReceivers(objectFactory.createMESSAGEReceivers());
+        request.setType(COMMAND);
+        if (name != null) {
+            request.setName(name);
+        }
+//        if (receivers != null) {
+//            for (String receiver : receivers) {
+//                request.getReceivers().getReceiver().add(receiver);
+//            }
+//        }
+        if (parameters != null) {
+
+            Set<String> paramKeys = parameters.keySet();
+            for (String key : paramKeys) {
+                MESSAGE.Parameters.Parameter msgParamter = objectFactory.createMESSAGEParametersParameter();
+                msgParamter.setName(key);
+                msgParamter.setValue(parameters.get(key));
+                request.getParameters().getParameter().add(msgParamter);
+            }
+        }
+        return request;
+    }
+
+    public static MESSAGE createMessage(String name, List<String> receivers, Map<String, String> parameters) {
+        ObjectFactory objectFactory = new ObjectFactory();
+        MESSAGE request = objectFactory.createMESSAGE();
+        request.setParameters(objectFactory.createMESSAGEParameters());
+        request.setReceivers(objectFactory.createMESSAGEReceivers());
+        request.setContents(objectFactory.createMESSAGEContents());
+        request.setType(MESSAGE);
+        if (name != null) {
+            request.setName(name);
+        }
+        if (receivers != null) {
+            for (String receiver : receivers) {
+                request.getReceivers().getReceiver().add(receiver);
+            }
+        }
+        if (parameters != null) {
+
+            Set<String> paramKeys = parameters.keySet();
+            for (String key : paramKeys) {
+                MESSAGE.Parameters.Parameter msgParamter = objectFactory.createMESSAGEParametersParameter();
+                msgParamter.setName(key);
+                msgParamter.setValue(parameters.get(key));
+                request.getParameters().getParameter().add(msgParamter);
+            }
+        }
+        return request;
+    }
+
+    public static MESSAGE createFiletransfer(List<String> receivers, byte[] data, Map<String, String> parameters) {
+        ObjectFactory objectFactory = new ObjectFactory();
+        MESSAGE request = objectFactory.createMESSAGE();
+        request.setParameters(objectFactory.createMESSAGEParameters());
+        request.setReceivers(objectFactory.createMESSAGEReceivers());
+        request.setType(REQUEST);
+        request.setName(Request.FILETRANSFER);
+
+        if (data != null) {
+            request.setData(data);
+        }
+
+        if (receivers != null) {
+            for (String receiver : receivers) {
+                request.getReceivers().getReceiver().add(receiver);
+            }
+        }
+        if (parameters != null) {
+
+            Set<String> paramKeys = parameters.keySet();
+            for (String key : paramKeys) {
+                MESSAGE.Parameters.Parameter msgParamter = objectFactory.createMESSAGEParametersParameter();
+                msgParamter.setName(key);
+                msgParamter.setValue(parameters.get(key));
+                request.getParameters().getParameter().add(msgParamter);
+            }
+        }
+        return request;
+    }
+
+    public static void addParameter(MESSAGE message, String name, String parameter) {
+        ObjectFactory objectFactory = new ObjectFactory();
+
+        MESSAGE.Parameters.Parameter msgParamter = objectFactory.createMESSAGEParametersParameter();
+        msgParamter.setName(name);
+        msgParamter.setValue(parameter);
+        message.getParameters().getParameter().add(msgParamter);
+    }
+
+    public static void addParameterAt(MESSAGE message, String name, String parameter, int position) {
+        ObjectFactory objectFactory = new ObjectFactory();
+
+        MESSAGE.Parameters.Parameter msgParamter = objectFactory.createMESSAGEParametersParameter();
+        msgParamter.setName(name);
+        msgParamter.setValue(parameter);
+        message.getParameters().getParameter().add(position, msgParamter);
+    }
+
+    public static void addReceiver(MESSAGE message, String receiver) {
+        message.getReceivers().getReceiver().add(receiver);
+    }
+
+    public static void addReceiverAt(MESSAGE message, String receiver, int position) {
+        message.getReceivers().getReceiver().add(position, receiver);
+    }
+
+    public static void addContent(MESSAGE message, String name, byte[] data) {
+        ObjectFactory objectFactory = new ObjectFactory();
+
+        MESSAGE.Contents.Content content = objectFactory.createMESSAGEContentsContent();
+        content.setName(name);
+        content.setValue(data);
+        message.getContents().getContent().add(content);
+    }
+
+    public static void addContentAt(MESSAGE message, String name, byte[] data, int position) {
+        ObjectFactory objectFactory = new ObjectFactory();
+
+        MESSAGE.Contents.Content content = objectFactory.createMESSAGEContentsContent();
+        content.setName(name);
+        content.setValue(data);
+        message.getContents().getContent().add(position, content);
+    }
+}
